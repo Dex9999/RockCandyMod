aecd39d4e8c8536c217c1ccc27a3898c776c3ed2
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-10-17 07:30:53 -0400

    all stuff

---
+++ Customs/Meth.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.References;
+using KitchenLib.Utils;
+using UnityEngine;
+
+namespace CoolMod.Customs
+{
+    public class MethDish : CustomDish
+    {
+        public override string UniqueNameID => "Meth";
+        public override bool IsAvailableAsLobbyOption => true;
+        public override Dictionary<Locale, string> Recipe => new Dictionary<Locale, string>
+        {
+            { Locale.English, "Walter, get in the kitchen!" }
+        };
+        public override HashSet<Process> RequiredProcesses => new HashSet<Process>
+        {
+            (Process)GDOUtils.GetExistingGDO(ProcessReferences.Chop)
+        };
+        public override GameObject IconPrefab => ((ItemGroup)GDOUtils.GetExistingGDO(ItemGroupReferences.Mayonnaise)).Prefab;
+        public override HashSet<Dish.IngredientUnlock> IngredientsUnlocks => new HashSet<Dish.IngredientUnlock>
+        {
+            new Dish.IngredientUnlock
+            {
+                MenuItem = (ItemGroup)GDOUtils.GetExistingGDO(ItemGroupReferences.Mayonnaise),
+                Ingredient = (Item)GDOUtils.GetExistingGDO(ItemReferences.Egg)
+            },
+            new Dish.IngredientUnlock
+            {
+                MenuItem = (ItemGroup)GDOUtils.GetExistingGDO(ItemGroupReferences.Mayonnaise),
+                Ingredient = (Item)GDOUtils.GetExistingGDO(ItemReferences.Oil)
+            }
+        };
+
+        public override HashSet<Item> MinimumIngredients => new HashSet<Item>()
+        {
+            (Item)GDOUtils.GetExistingGDO(ItemReferences.Oil),
+            (Item)GDOUtils.GetExistingGDO(ItemReferences.Egg)
+        };
+
+        public override List<Dish.MenuItem> ResultingMenuItems => new List<Dish.MenuItem>
+        {
+            new Dish.MenuItem
+            {
+                Item = (ItemGroup)GDOUtils.GetExistingGDO(ItemGroupReferences.Mayonnaise),
+                Phase = MenuPhase.Main,
+                Weight = 1,
+                DynamicMenuType = DynamicMenuType.Static,
+                DynamicMenuIngredient = null
+            }
+        };
+
+        public override List<string> StartingNameSet => new List<string> {
+            "Mr White's Delights",
+            "Blue Sky",
+            "Los Pollos Hermanos"
+        };
+        public override DishType Type => DishType.Base;
+        public override int BaseGameDataObjectID => DishReferences.BurgerBase;
+
+        //CustomUnlock
+        public override Unlock.RewardLevel ExpReward => Unlock.RewardLevel.Medium;
+        public override bool IsUnlockable => true;
+        public override UnlockGroup UnlockGroup => UnlockGroup.Dish;
+        public override CardType CardType => CardType.Default;
+        public override int MinimumFranchiseTier => 0;
+        public override bool IsSpecificFranchiseTier => false;
+        public override DishCustomerChange CustomerMultiplier => DishCustomerChange.SmallDecrease;
+        public override float SelectionBias => 0;
+        public override List<Unlock> HardcodedRequirements => new List<Unlock> { };
+        public override List<Unlock> HardcodedBlockers => new List<Unlock> { };
+
+        public override void OnRegister(GameDataObject gameDataObject)
+        {
+            Dish dish = (Dish)gameDataObject;
+            LocalisationObject<UnlockInfo> info = new LocalisationObject<UnlockInfo>();
+            FieldInfo dictionary = ReflectionUtils.GetField<LocalisationObject<UnlockInfo>>("Dictionary");
+            Dictionary<Locale, UnlockInfo> dict = new Dictionary<Locale, UnlockInfo>();
+            dict.Add(Locale.English, new UnlockInfo
+            {
+                Name = "Meth",
+                Description = "Adds Meth as a Main Dish"
+            });
+            dictionary.SetValue(info, dict);
+            dish.Info = info;
+
+            ((ItemGroup)GDOUtils.GetExistingGDO(ItemGroupReferences.Mayonnaise)).SatisfiedBy.Add((ItemGroup)GDOUtils.GetExistingGDO(ItemGroupReferences.Mayonnaise));
+        }
+    }
+}
---
+++ mod.cs
@@ -0,0 +1,52 @@
+﻿using KitchenLib;
+using CoolMod.Customs;
+using KitchenMods;
+using System.Reflection;
+using UnityEngine;
+
+namespace Meth
+{
+    public class Mod : BaseMod, IModSystem
+    {
+        public const string MOD_GUID = "com.icecreamsandwch.breakingkitchen";
+        public const string MOD_NAME = "Cook Meth";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "IceCreamSandwch";
+        public const string MOD_GAMEVERSION = ">=1.1.7";
+
+        public static AssetBundle Bundle;
+
+        public Mod() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+
+        protected override void OnInitialise()
+        {
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+        }
+
+        private void AddGameData()
+        {
+            LogInfo("Attempting to register game data...");
+
+            AddGameDataObject<MethDish>();
+
+            LogInfo("Done loading game data.");
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+
+        protected override void OnPostActivate(KitchenMods.Mod mod)
+        {
+            AddGameData();
+        }
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
+    }
+}
